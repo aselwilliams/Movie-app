9448e5929d5c9c2a3524098c58d262c403dd5bc4
aselwilliams <aselwilliams22@gmail.com>
2022-07-25 02:11:11 -0500

    created component, fetched data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import {useState, useEffect } from 'react'
-import './App.css';
-import Movie from './components/Movie';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Movie from "./components/Movie";
+
+const featured_api =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const img_api = "https://image.tmdb.org/t/p/w1280";
+const search_api =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
-  const movies=['1','2','3']
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(featured_api)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setMovies(data.results);
+      });
+  }, []);
+  console.log(movies);
   return (
     <div className="App">
-      {movies.map((movie)=>(
+      {movies.map((movie) => (
         <Movie />
       ))}
     </div>
