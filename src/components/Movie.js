cadcf1da3754848097471bbba6ec7ba9c9b1fed5
aselwilliams <aselwilliams22@gmail.com>
2022-07-25 11:46:06 -0500

    movie rating conditionally styled

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,22 @@
 const img_api = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass=(vote)=>{
+if(vote>=8){
+    return 'green'
+} else if(vote>=6){
+    return 'orange'
+} else {
+    return 'red'
+}
+}
+
 const Movie=({title, poster_path, overview, vote_average})=>{
     return(
         <div className="movie">
             <img src={img_api+poster_path} />
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
             </div>
 
             <div className="movie-overview">
