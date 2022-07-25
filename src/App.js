87d4146948d32716ea8932d483386e6802842391
aselwilliams <aselwilliams22@gmail.com>
2022-07-25 01:45:30 -0500

    basic style added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {useState, useEffect } from 'react'
 import './App.css';
+import Movie from './components/Movie';
 
 function App() {
+  const movies=['1','2','3']
   return (
     <div className="App">
- 
+      {movies.map((movie)=>(
+        <Movie />
+      ))}
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,9 @@
+
+const Movie=()=>{
+    return(
+        <div className="movie">
+            This is a movie component
+        </div>
+    )
+}
+export default Movie;
